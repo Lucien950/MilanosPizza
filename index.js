8d100aaaa0cfb737dcd5b8e4dbfaac2980fc28b2
Edwin <jessicawu414@gmail.com>
2020-09-20 11:58:21 -0400

    More Footer Things, and Scaled

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ Vue.component('food-card', {
 Vue.component('footer-col', {
     template: "#footer-col-template"
 })
+Vue.component('social-icon', {
+    template: "#social-icon-template",
+    props: ['idClass', 'url']
+})
 
 mainApp = new Vue({
     el: "#app",
@@ -22,6 +26,35 @@ mainApp = new Vue({
                 price: '32.42',
                 description: 'This is also very good'
             },
+        ],
+
+        socialItems:[
+            {
+                idClass:"fa-twitter-square",
+                url:"https://twitter.com/milanopizza1"
+            },
+            {
+                idClass:"fa-facebook-square",
+                url:"https://www.facebook.com/MilanoPizzeriaBarrhaven/"
+            },
+            {
+                idClass:"fa-instagram-square",
+                url:"https://www.instagram.com/milanopizza1/"
+            },
+            {
+                idClass:"fa-google-plus-square",
+                url:"https://plus.google.com/113931051887402511809"
+            },
+            {
+                idClass:"fa-yelp",
+                url:"https://www.yelp.ca/biz/milano-pizzeria-nepean"
+            },
         ]
     }
 })
+
+
+function openInNewTab(url) {
+    var win = window.open(url, '_blank');
+    win.focus();
+}
