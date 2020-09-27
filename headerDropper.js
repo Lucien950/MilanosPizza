1c95a3fcff0780e56ad656bba684e58ada54bc0a
Edwin <jessicawu414@gmail.com>
2020-09-26 22:34:38 -0400

    Like navbar I think?

--- headerDropper.js
+++ headerDropper.js
@@ -1,23 +1,52 @@
 const anchors = document.querySelectorAll("#linkRow a");
-console.log(anchors);
+const pages = document.querySelectorAll(".page");
+console.log("anchors", anchors);
+console.log("pages", pages);
 
+const pageObserver = new IntersectionObserver(
+    (entries, pageObserver)=>{
+        entries.forEach(entry => {
+            //console.log(entry.target.id, entry.isIntersecting ? "Is Entering" : "Is Leaving");
+            const anchorNum = Array.from(pages).findIndex(page => page == entry.target);
+            const pageNavAnchor = anchors[anchorNum];
+
+            if(entry.isIntersecting){
+                pageNavAnchor.classList.add("activeNav");
+            } 
+            else if (pageNavAnchor.classList.contains("activeNav")){
+                pageNavAnchor.classList.remove("activeNav");
+            }
+        })
+    },
+    {
+        rootMargin:"-50%"
+    }
+)
+pages.forEach(page => {
+    pageObserver.observe(page);
+    console.log(page, "is being observed")
+})
 
 
 // HEADER
 const header = document.querySelector("nav");
-const headerActivator = document.querySelector("#mainText");
-const headerOptions = {
-    root:null,
-    threshhold:0,
-    rootMargin:"-49.2%"
-};
-const headerObserver = new IntersectionObserver(function (entries, headerObserver) {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            header.classList.add("navScrolled");
-        } else {
-            header.classList.remove("navScrolled");
-        }
-    })
-}, headerOptions);
-headerObserver.observe(headerActivator);
+const headerObserver = new IntersectionObserver(
+    //MAIN FUNCTION
+    function (entries, headerObserver) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                header.classList.add("navScrolled");
+            } else {
+                header.classList.remove("navScrolled");
+            }
+        })
+    },
+    // OPTIONS
+    {
+        root: null,
+        threshhold: 0,
+        rootMargin: "-49.2%"
+    }
+);
+
+headerObserver.observe(document.querySelector("#mainText"));
--- index.js
+++ index.js
@@ -71,6 +71,20 @@ mainApp = new Vue({
                 'imageurl': "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5f4ebe0c87612dab4f12a597%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D292%26cropX2%3D3684%26cropY1%3D592%26cropY2%3D3987",
                 'review': "This was a very ok pizza I really oked with it could do with more parmesean because I am a freak"
             }
+        ],
+        aboutUsImages:[
+            {
+                'imageURL':'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
+                'alt':'tree'
+            },
+            {
+                'imageURL': 'https://www.w3schools.com/w3css/img_lights.jpg',
+                'alt': 'Lights'
+            },
+            {
+                'imageURL': 'https://45jo5o2sxknq2fucgi3kwxpa-wpengine.netdna-ssl.com/wp-content/uploads/2018/05/4-3-Aspect-Ratio-vs-185-235-1024x436.jpg',
+                'alt': 'Sunny'
+            }
         ]
     }
 })
@@ -110,4 +124,9 @@ reviewBoxes.forEach((review, i) => {
         console.log("OH Snap you fucked up")
     }
     
+});
+
+const imagery = document.querySelector("#imagery");
+window.addEventListener("scroll", ()=>{
+    imagery.style.top = (scrollY/-3)+"px";
 });
